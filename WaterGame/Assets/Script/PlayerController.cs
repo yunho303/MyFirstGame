180627379c60b4ea0b1d466cf14f59733d2b3881
yunho303 <pizza303@naver.com>
2023-09-27 15:57:13 +0900

    Water, Squid clear

--- WaterGame/Assets/Script/PlayerController.cs
+++ WaterGame/Assets/Script/PlayerController.cs
@@ -1,31 +1,52 @@
+using Unity.VisualScripting;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
-    public float rollAmount;
-    public float pitchAmount;
-    public float yawAmount;
+
+    public float rotateSpeed;
+
+    float hAxis;
+    float vAxis;
     Rigidbody rb;
-    Vector3 rotateValue;
+    public float mousex;
+    public float mousey;
     // Start is called before the first frame update
     void Start()
     {
-        
+         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void FixedUpdate() {
+       
         Move();
     }
 
+    
+
     void Move(){
         //rotateValue = new Vector3(pitchAmount)
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
+        //mousex = Input.GetAxis("Mouse X");
+        //mousey = Input.GetAxis("Mouse Y");
+        //Vector3 dir = new Vector3(hAxis,vAxis,0).normalized;
+        Vector3 rotateValue = new Vector3(vAxis * rotateSpeed, 
+                              hAxis * rotateSpeed, 
+                              0);
+                              
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateValue * Time.deltaTime));
+
+
+        //transform.Rotate(new Vector3(0,hAxis,0) * rotateSpeed *Time.deltaTime, Space.World); 
+        
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.AddForce(10 * transform.forward,ForceMode.Impulse);
+        }
     }
 }
